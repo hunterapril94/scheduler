694c2590ee058728ae3095932490da9a9bf878cf
April Larsen <hunterapril94@gmail.com>
2021-12-07 17:35:16 -0800

    add button and daylistitem functionalities

---
+++ src/components/DayListItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "components/DayListItem.scss"
+import classNames from "classnames";
+
+export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {"day-list__item--selected": props.selected}, {"day-list__item--full":(props.spots===0)})
+  const formatSpots = () => {
+    let result = "";
+    if(props.spots > 1) {
+      result = `${props.spots} spots remaining`
+    } else if(props.spots === 1) {
+      result= `1 spot remaining`
+    } else if(props.spots === 0) {
+      result = "no spots remaining"
+    }
+    return result
+  }
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots()}</h3>
+    </li>
+  )
+}
---
+++ src/components/Daylist.js
@@ -0,0 +1,8 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+function Daylist(props) {
+  return(
+    <ul></ul>
+  )
+}
