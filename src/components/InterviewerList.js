ce7646cc1708605788a3248e8bdd9e6715e6a329
April Larsen <hunterapril94@gmail.com>
2021-12-08 17:52:29 -0800

    add interviewer list and populate

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerList.scss"
+
+export default function InterviewerList(props) {
+  const interviewers = props.interviewers.map((i)=>{
+    return(
+      <InterviewerListItem 
+        name={i.name}
+        key={i.id}
+        avatar={i.avatar}
+        selected={i.id===props.interviewer}
+        setInterviewer={()=> props.setInterviewer(i.id)}
+      />
+    )
+
+  })
+  return(
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewers}</ul>
+    </section>
+  )
+}
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,23 @@
+// import { useState } from "react";
+import React from "react";
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
+// const interviewer = {
+//   id: 1,
+//   name: "Sylvia Palmer",
+//   avatar: "https://i.imgur.com/LpaY82x.png"
+// };
+
+export default function InterviewerListItem(props) {
+  const interviewerClass = classNames("interviewers__item", {"interviewers__item--selected": props.selected})
+  return(
+    <li className={interviewerClass} onClick={props.setInterviewer} >
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && <p>{props.name}</p>}
+    </li>
+  )
+}
