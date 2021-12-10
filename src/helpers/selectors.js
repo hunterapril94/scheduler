008aa9af541bc16645e6b15a29277e78fd5063b7
April Larsen <hunterapril94@gmail.com>
2021-12-09 21:30:33 -0800

    add helper function to select appointments based on day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,18 @@
+export function getAppointmentsForDay(state, day) {
+  let correctDay={};
+  let appointments=[];
+  for(let i=0; i<state.days.length; i++) {
+    if(state.days[i].name === day) {
+      correctDay=state.days[i]
+    }
+  }
+  console.log(correctDay);
+  console.log(correctDay.appointments)
+  if(correctDay && correctDay.appointments) {
+    for(let i=0; i < correctDay.appointments.length; i++) {
+      let id = correctDay.appointments[i]
+      appointments.push(state.appointments[`${id}`])
+    }
+  }
+  return appointments
+}
